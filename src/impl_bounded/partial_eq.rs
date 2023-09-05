038b61b26d17e7f6e89d08aca369083c345096b8
Ugur-Arikan <uurfun@gmail.com>
2023-09-05 19:23:11 +0200

    iter-rev and test coverage
    Reverse iterator is implemented. It is actually a regular iterator starting from backwards, rather than a double-ended iterator.
    
    Tests are implemented.

--- src/impl_bounded/partial_eq.rs
+++ src/impl_bounded/partial_eq.rs
@@ -9,6 +9,8 @@ where
     /// Returns `true` if the `LinkedList` contains an element equal to the
     /// given value.
     ///
+    /// Note that, as usual, this method compares value equality.
+    ///
     /// This operation should compute linearly in *O*(*n*) time.
     ///
     /// # Examples
@@ -16,7 +18,7 @@ where
     /// ```
     /// use orx_linked_list::prelude::*;
     ///
-    /// let mut list = LinkedList::with_doubling_growth(8);
+    /// let mut list = LinkedList::with_doubling_growth(8, MemoryUtilization::default());
     /// list.push_back(0);
     /// list.push_back(1);
     /// list.push_back(2);
@@ -24,7 +26,36 @@ where
     /// assert_eq!(list.contains(&0), true);
     /// assert_eq!(list.contains(&10), false);
     /// ```
-    pub fn contains(&'a self, x: &T) -> bool {
+    pub fn contains(&self, x: &T) -> bool {
         self.iter().any(|e| e == x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn contains() {
+        let mut list = LinkedList::with_linear_growth(8, crate::MemoryUtilization::Eager);
+
+        assert!(!list.contains(&'x'));
+
+        list.push_back('y');
+        assert!(!list.contains(&'x'));
+
+        list.push_back('x');
+        assert!(list.contains(&'x'));
+
+        list.push_front('z');
+        assert!(list.contains(&'x'));
+
+        // z -> y -> x
+
+        _ = list.pop_front();
+        assert!(list.contains(&'x'));
+
+        _ = list.pop_back();
+        assert!(!list.contains(&'x'));
     }
 }
