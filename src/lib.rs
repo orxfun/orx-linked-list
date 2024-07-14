e1e07cea8d564b643cc0f380061c34539c954e41
Ugur Arikan (DHL Data & Analytics) <ugur.arikan@dpdhl.com>
2024-07-14 22:20:35 +0200

    contributions added

--- src/lib.rs
+++ src/lib.rs
@@ -325,6 +325,10 @@
 //!
 //! <img src="https://raw.githubusercontent.com/orxfun/orx-linked-list/main/docs/img/iter.PNG" alt="https://raw.githubusercontent.com/orxfun/orx-linked-list/main/docs/img/iter.PNG" />
 //!
+//! ## Contributing
+//!
+//! Contributions are welcome! If you notice an error, have a question or think something could be improved, please open an [issue](https://github.com/orxfun/orx-linked-list/issues/new) or create a PR.
+//!
 //! ## License
 //!
 //! This library is licensed under MIT license. See LICENSE for details.
